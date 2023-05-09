eaffdc188204e5a09c4ef6124dc8c13f0bcddc5b
Ashail33 <34643237+Ashail33@users.noreply.github.com>
2023-05-09 11:45:32 +0100

    Update generate.py

--- mdcgenpy/clusters/generate.py
+++ mdcgenpy/clusters/generate.py
@@ -132,15 +132,16 @@ def generate_clusters(clus_cfg, batch_size = 0, output_file=None):
     for batch in range(((clus_cfg.n_samples - 1) // batch_size) + 1):
         n_samples = min(batch_size, clus_cfg.n_samples - (batch * batch_size))
         data, labels = compute_batch(clus_cfg, n_samples)
-        if output_file is not None:
-            # Combine data and labels
-            combined_data = np.hstack((data, np.reshape(labels, (len(labels), 1))))
-            
-            # Save combined data and labels to file
-            with open(output_file, "ab") as f_output:
-                np.savetxt(f_output, combined_data, delimiter=",")
-            print('saved')
         yield data, np.reshape(labels, (len(labels), 1))
+    if output_file is not None:
+        # Combine data and labels
+        combined_data = np.hstack((data, np.reshape(labels, (len(labels), 1))))
+
+        # Save combined data and labels to file
+        with open(output_file, "ab") as f_output:
+            np.savetxt(f_output, combined_data, delimiter=",")
+            print('saved')
+    
 
 
 def get_rotation_matrix(n_feats):
